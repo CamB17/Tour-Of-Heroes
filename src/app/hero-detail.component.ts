cc87e4e0e477d278a66c739e97ad7aae0f66fa78
CamB17 <camcodes@Camerons-MacBook-Pro.local>
2017-05-17 09:03:12 -0600

    not done

---
+++ src/app/hero-detail.component.ts
@@ -0,0 +1,24 @@
+//new HeroDetailComponent here
+
+import { Component } from '@angular/core';
+import { Hero } from './hero'
+
+//This defines a component
+@Component({
+  selector: 'hero-detail',
+  template: `
+    <div *ngIf="hero">
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name"/>
+      </div>
+    </div>
+  `
+})
+
+export class HeroDetailComponent {
+  hero: Hero;
+
+}
